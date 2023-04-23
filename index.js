f59ffb71e2d2093f0193a45e3e0cc32f8efd6220
SEOForge <121815475+Seoforge@users.noreply.github.com>
2023-04-23 20:58:30 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -10,34 +10,38 @@ const client = new Client({
 });
 
 // setting up open ai
-const {Configuration, OpenAIApi} = require("openai");
+const { Configuration, OpenAIApi } = require("openai");
 
-const configuration = new Configuration ({ 
-             organization:process.env.OPENAI_ORG, 
-             apiKey:process.env.OPENAI_KEY
-})
+const configuration = new Configuration({
+  organization: process.env.OPENAI_ORG,
+  apiKey: process.env.OPENAI_KEY,
+});
 
-const openai = new OpenAIApi(configuration) 
+const openai = new OpenAIApi(configuration);
 
-client.on('messageCreate', async function(message) {
-    try {
+client.on("messageCreate", async function (message) {
+  try {
+    if (message.author.bot) return;
+    // const completion = await openai.createChatCompletion({
 
-        if(message.author.bot) return;
-        const response = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: `Chat with Critic is a friendly chatbot, created by Critic Designs's Team : ${message.content}`,
-            temperature: 0.5,
-            max_tokens: 30,
-            top_p: 1.0,
-            frequency_penalty: 0.5,
-            presence_penalty: 0.0,
-          });
-
-          message.reply(`${response.data.choices[0].text}`)
-   
-    } catch (error) {
-        console.log(error)
-}
-})
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "user",
+          content: `Chat with Critic is a friendly chatbot, created by Critic Designs's Team : ${message.content}`,
+        },
+      ],
+      max_tokens: 500,
+      top_p: 1.0,
+    });
+    // console.log(response.data);
+    res = response.data.choices[0].message["content"];
+    console.log(`${res}`);
+    message.reply(`${res}`);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 client.login(process.env.DISCORD_KEY);
