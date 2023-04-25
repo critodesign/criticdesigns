43c7602fd892d668986e6efabdf5caec10f4c943
SEOForge <121815475+Seoforge@users.noreply.github.com>
2023-04-25 12:40:48 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -18,18 +18,24 @@ const configuration = new Configuration({
 });
 
 const openai = new OpenAIApi(configuration);
+function checkParagraphLength(paragraph) {
+  if (paragraph.length > 2000) {
+    return false;
+  } else {
+    return true;
+  }
+}
 
-client.on("messageCreate", async function (message) {
+async function openaihandler(message) {
   try {
     if (message.author.bot) return;
-    // const completion = await openai.createChatCompletion({
 
     const response = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
       messages: [
         {
           role: "system",
-          content: `I'm a happy chatbot named Crito, created by the Critic Designs Community.`,
+          content: `Crito has just been activated as your personal AI assistant. As an advanced language model developed by Critic Designs which was founded and Owned By Himanshu Raikwar. , Get Into The Design World With Critic Designs\n\nOur goal at Critic Designs is to help you advance in your design career, and to make lifelong friends along the way.\n\nThings we provide to our members:\n- Mentorship: You can make your career path very easy by connecting with a perfect mentor in Critic Designs\n- Portfolio Help: Get a portfolio review by an industry professional, which can help you improve your portfolio.\n- Make Friends: Friendships have a huge impact on your mental health and happiness. Good friends relieve stress.\n- Live Chat: Talk to fellow designers about new design trends and grow your skills in Critic Designs Community. Crito is here to help. Just type in your request and let Crito take care of the rest!`,
         },
         {
           role: "user",
@@ -42,10 +48,26 @@ client.on("messageCreate", async function (message) {
     // console.log(response.data);
     res = response.data.choices[0].message["content"];
     console.log(`${res}`);
-    message.reply(`${res}`);
+    bo = checkParagraphLength(res);
+    if (bo == true) {
+      message.reply(`${res}`);
+    } else {
+      message.reply("Sorry, Discord doesn't want me to Help You");
+    }
   } catch (error) {
-    console.log(error);
+    console.error("An error occurred while processing a message:", error);
+    message.reply("Sorry, something went wrong while processing your message.");
   }
+}
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on("messageCreate", openaihandler);
+
+client.on("error", (error) => {
+  console.error("An error occurred:", error);
 });
 
 client.login(process.env.DISCORD_KEY);
